36e9d3c936979ebce771056a7c2eb928c11581d8
AliaK77 <aliak77@gmail.com>
2022-12-01 03:02:21 +1100

    0301 exercise6 done

--- main.js
+++ main.js
@@ -2,8 +2,28 @@ const app = Vue.createApp({ // keep { on this line not newline
         data(){
             return {
                 product: 'socks and turtles',
-                image: './assets/images/socks_blue.jpg'
+                image: './assets/images/socks_green.jpg',
+                inStock: true,
+                inventory: 1,
+                onSale: false,
+                details: ['50% cotton', '30% wool', '20% polyester'],
+                variants: [
+                    { id: 2234, colour: 'green', image: './assets/images/socks_green.jpg' },
+                    {id: 2235, colour: 'blue', image: './assets/images/socks_blue.jpg' }
+                ],
+                cart:0 // initialise cart 
             } // end return
-        } // end data()
+        }, // end data()
 
+        methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            removeFromCart() {
+                this.cart -= 1
+            },
+            updateImage(variantImage) {
+                this.image = variantImage
+            }
+        }
     })  // end app createApp
