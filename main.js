a81fba0d244c03ed0064a4cb97856ccac65cb467
AliaK77 <aliak77@gmail.com>
2022-12-01 01:49:23 +1100

    0149 fixed missing > & added div comments

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({ // keep { on this line not newline
         data(){
             return {
                 product: 'socks and turtles',
-                image: './assets/images/socks_green.jpg'
+                image: './assets/images/socks_blue.jpg'
             } // end return
         } // end data()
 
